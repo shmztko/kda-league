1b6e93e3e1a11bb4c5641265bc2d5c9c46bc9db5
takewo <st0098@gmail.com>
2013-03-19 15:22:57 +0900

    Initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KdaLeague::Application.config.secret_token = 'be0edab3bb4caa69358d8683ba9ae531fc78418b4e39068848e0b3166a78eb0f06d420094173221f557085a5e1a802c0b3bcd5b9f6d842bdc9bb2d028dc3b073'
