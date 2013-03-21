47e323abdde137d78dfcec66961c612c53f2f091
takewo <st0098@gmail.com>
2013-03-21 18:54:23 +0900

    ゲーム結果のCRUD機能はだいたい追加

--- app/controllers/game_results_controller.rb
+++ app/controllers/game_results_controller.rb
@@ -1,83 +1,62 @@
 class GameResultsController < ApplicationController
   # GET /game_results
-  # GET /game_results.json
   def index
     @game_results = GameResult.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @game_results }
-    end
   end
 
   # GET /game_results/1
-  # GET /game_results/1.json
   def show
     @game_result = GameResult.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @game_result }
-    end
   end
 
   # GET /game_results/new
-  # GET /game_results/new.json
   def new
     @game_result = GameResult.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game_result }
-    end
+    @shops = Shop.all
+    @winning_types = WinningType.all
   end
 
   # GET /game_results/1/edit
   def edit
     @game_result = GameResult.find(params[:id])
+    @shops = Shop.all
+    @winning_types = WinningType.all
   end
 
   # POST /game_results
-  # POST /game_results.json
   def create
-    @game_result = GameResult.new(params[:game_result])
-
-    respond_to do |format|
-      if @game_result.save
-        format.html { redirect_to @game_result, notice: 'Game result was successfully created.' }
-        format.json { render json: @game_result, status: :created, location: @game_result }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game_result.errors, status: :unprocessable_entity }
-      end
+    @game_result = GameResult.new(prepare(params)[:game_result])
+    if @game_result.save
+      redirect_to @game_result, notice: 'Game result was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /game_results/1
-  # PUT /game_results/1.json
   def update
     @game_result = GameResult.find(params[:id])
 
-    respond_to do |format|
-      if @game_result.update_attributes(params[:game_result])
-        format.html { redirect_to @game_result, notice: 'Game result was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @game_result.errors, status: :unprocessable_entity }
-      end
+    if @game_result.update_attributes(prepare(params)[:game_result])
+      redirect_to @game_result, notice: 'Game result was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
   # DELETE /game_results/1
-  # DELETE /game_results/1.json
   def destroy
     @game_result = GameResult.find(params[:id])
     @game_result.destroy
+    redirect_to @game_result
+  end
 
-    respond_to do |format|
-      format.html { redirect_to game_results_url }
-      format.json { head :no_content }
-    end
+
+private
+  def prepare(params)
+    params[:game_result][:home_shop] = Shop.find(params[:game_result][:home_shop])
+    params[:game_result][:away_shop] = Shop.find(params[:game_result][:away_shop])
+    params[:game_result][:winning_type] = WinningType.find(params[:game_result][:winning_type])
+    params
   end
 end
--- app/models/game_result.rb
+++ app/models/game_result.rb
@@ -1,5 +1,6 @@
 class GameResult < ActiveRecord::Base
   attr_accessible :away_score, :home_score, :scheduled_at
+  attr_accessible :home_shop, :away_shop, :winning_type
   belongs_to :home_shop, :foreign_key => :home_shop_id, :class_name => 'Shop'
   belongs_to :away_shop, :foreign_key => :away_shop_id, :class_name => 'Shop'
   belongs_to :winning_type
