4a1cf06b49a01213ff7b626133f0dfdd8768b78d
takewo <st0098@gmail.com>
2013-04-05 18:45:21 +0900

    プレイヤー情報を追加更新できるようにした

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,71 +1,44 @@
 class PlayersController < ApplicationController
   # GET /players
-  # GET /players.json
   def index
     @players = Player.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @players }
-    end
   end
 
   # GET /players/1
-  # GET /players/1.json
   def show
     @player = Player.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @player }
-    end
   end
 
   # GET /players/new
-  # GET /players/new.json
   def new
     @player = Player.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @player }
-    end
+    @shops = Shop.all
   end
 
   # GET /players/1/edit
   def edit
     @player = Player.find(params[:id])
+    @shops = Shop.all
   end
 
   # POST /players
-  # POST /players.json
   def create
-    @player = Player.new(params[:player])
+    @player = Player.new(prepare_for_save(params)[:player])
 
-    respond_to do |format|
-      if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
-        format.json { render json: @player, status: :created, location: @player }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
+    if @player.save
+      redirect_to @player, notice: 'Player was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /players/1
-  # PUT /players/1.json
   def update
     @player = Player.find(params[:id])
-
-    respond_to do |format|
-      if @player.update_attributes(params[:player])
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
+    if @player.update_attributes(prepare_for_save(params)[:player])
+      redirect_to @player, notice: 'Player was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -74,10 +47,12 @@ class PlayersController < ApplicationController
   def destroy
     @player = Player.find(params[:id])
     @player.destroy
+    redirect_to players_url
+  end
 
-    respond_to do |format|
-      format.html { redirect_to players_url }
-      format.json { head :no_content }
-    end
+private
+  def prepare_for_save(params)
+    params[:player][:shop] =  params[:player][:shop].empty? ? nil : Shop.find(params[:player][:shop])
+    return params
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ActiveRecord::Base
   belongs_to :shop
-  attr_accessible :name
+  attr_accessible :name, :shop
 end
