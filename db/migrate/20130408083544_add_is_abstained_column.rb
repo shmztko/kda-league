2c4cf91660bd08ad7e49c392b7697a53ce38afc6
takewo <st0098@gmail.com>
2013-04-09 17:29:24 +0900

    棄権フラグの移行ファイルと移行済みデータ

--- db/migrate/20130408083544_add_is_abstained_column.rb
+++ db/migrate/20130408083544_add_is_abstained_column.rb
@@ -2,7 +2,7 @@ class AddIsAbstainedColumn < ActiveRecord::Migration
   def change
   	add_column :games, :is_abstained, :boolean, :default => false
     
-    execute  "UPDATE games SET is_abstained=0
+    execute  "UPDATE games SET is_abstained=1
       WHERE winning_type_id IN (
         SELECT id FROM winning_types WHERE kind LIKE '%default%'
       )"
