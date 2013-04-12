a0df52a2121b4b0c234b3358936c5afc8bc6814a
takewo <st0098@gmail.com>
2013-04-12 19:23:14 +0900

    Made fixture files. then wrote spec for Game.rb

--- app/models/game_detail.rb
+++ app/models/game_detail.rb
@@ -1,7 +1,8 @@
 class GameDetail < ActiveRecord::Base
   attr_accessible :legs, :throws
 
-  belongs_to :game, :shop
+  belongs_to :game
+  belongs_to :shop
 
   has_many :players, :through => 'entried_palyers'
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,14 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe Game do
-  
+  fixtures :all
+
+  before do
+    @game = games(:game1)
+  end
+
+  it 'の詳細は複数取得できること' do
+    @game.should have_at_least(2).game_details      
+  end
 end
